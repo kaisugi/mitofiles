9373322a80f78a0004fc0e49e7561fdb1bc2ecff
7ma7X <hellorusk1998@gmail.com>
2020-06-09 20:18:53 +0900

    update homebrew formulae

--- brew.sh
+++ brew.sh
@@ -10,6 +10,7 @@ brew install gcc
 brew install gist
 brew install git
 brew install gnupg 
+brew install go
 brew install keybase
 brew install ocaml
 brew install opam
@@ -20,10 +21,15 @@ brew install tmux
 brew install wget
 brew install wifi-password
 
+brew cask install authy
+brew cask install deepl
+brew cask install firefox
 brew cask install github
 brew cask install google-chrome
+brew cask install iterm2
 brew cask install keybase
 brew cask install slack
+brew cask install spotify
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install visual-studio-code
