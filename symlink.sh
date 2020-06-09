4edce6b0f8334ad4694b419dcb1e5473a362f0f5
7ma7X <hellorusk1998@gmail.com>
2020-06-09 23:00:19 +0900

    rename vim script

--- symlink.sh
+++ symlink.sh
@@ -2,4 +2,4 @@ ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vim/sysinit.vim ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
