fe413934e893f1c186e1450adc3c219503777e7c
7ma7X <hellorusk1998@gmail.com>
2020-06-17 09:11:26 +0900

    add .nvmrc

--- init.sh
+++ init.sh
@@ -14,8 +14,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
-nvm use node
+echo "lts/*" > .nvmrc
+nvm install
+nvm use
 
 # Yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
