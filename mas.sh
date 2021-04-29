1ea9432a7beb971744a7c13f193bf195c0adfa39
HelloRusk <hellorusk1998@gmail.com>
2021-04-30 08:49:34 +0900

    add TinyStopwatch

--- mas.sh
+++ mas.sh
@@ -4,3 +4,4 @@ mas install 414298354  # ToyViewer
 mas install 539883307  # LINE
 mas install 1153157709 # Speedtest by Ookla
 mas install 1278508951 # Trello
+mas install 1447754003 # TinyStopwatch
