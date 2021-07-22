ce6d407eeb5412224b97c988ba916d8678d3b6ce
HelloRusk <hellorusk1998@gmail.com>
2021-07-22 16:25:21 +0900

    add kindle

--- mas.sh
+++ mas.sh
@@ -1,5 +1,6 @@
 # Install apps provided in Mac App Store
 
+mas install 405399194  # Kindle
 mas install 414298354  # ToyViewer
 mas install 539883307  # LINE
 mas install 1153157709 # Speedtest by Ookla
